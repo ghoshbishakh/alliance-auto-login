c06b099edc1b9033bd9481c506edb4adb1289cd5
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-05-08 20:54:34 +0530

    Use flexible login urls

--- alliance_auto_login/autologin.py
+++ alliance_auto_login/autologin.py
@@ -5,36 +5,43 @@ import getpass
 from bs4 import BeautifulSoup
 
 
-url = "http://10.254.254.45/0/up/"
 
 def print_usage():
-    print("Usage: allianceautologin [\'--login\' or \'-l\'] \n allianceautologin [\'--status\' or \'-s\'] ")
+    print("Usage: allianceautologin [\'--login\' or \'-l\'] \n \
+           allianceautologin [\'--status\' or \'-s\'] ")
 
 
-def login(username, password):
+def login(url, username, password):
     print("Attempting to login..")
     payload = {'user': username,
                'pass': password,
                'login': 'Login'}
     try:
         requests.post(url, data=payload)
+        print("Logged In")
     except:
         print("Could not connect to alliance server")
 
 
 def login_status():
-    r = requests.get('https://news.ycombinator.com/', timeout=5)
-    if(r.status_code != 200):
-        login(username, password)
-    else:
-        print("Logged in")
+    try:
+        r = requests.get('https://news.ycombinator.com/', timeout=5)
+        if(r.status_code != 200):
+            print("Not Logged In")
+            return False
+        else:
+            print("Logged In")
+            return True
+    except:
+        print("Not Logged In")
+        return False
 
 
-def get_service_status():
+def get_service_status(url):
     r = requests.get(url).text
     soup = BeautifulSoup(r, "lxml")
 
-    soup = soup.find('table', attrs = {'id': 'thesmalltable'})
+    soup = soup.find('table', attrs={'id': 'thesmalltable'})
     rows = soup.find_all('tr')
 
     # show only name, client id, package and expiry date
@@ -48,38 +55,58 @@ def get_service_status():
     print("EXPIRY: ", expiry)
 
 
-def service_status(username, password):
+def service_status(url, username, password):
     """
     Shows status of service
     """
-
-    # log in if not already 
-    login_status()
-    get_service_status() 
+    # log in if not already
+    login(url, username, password)
+    get_service_status(url)
 
 
 def main():
     if len(sys.argv) < 2:
         print_usage()
         return
-    
-    if sys.argv[1] != '--login' and sys.argv[1] != '-l' and sys.argv[1] != '--status' and sys.argv[1] != '-s':
-        print_usage()
-        return
 
-    username = input('Enter username: ')
-    password = getpass.getpass('Enter password: ')
-    
     try:
-        if sys.argv[1] == '--login' or sys.argv[1] == '-l':
+        if (len(sys.argv) == 5 and
+                (sys.argv[1] == '--login' or sys.argv[1] == '-l')):
+            url = sys.argv[2]
+            username = sys.argv[3]
+            password = sys.argv[4]
+
             while(1):
-                try:
-                    login_status()
-                except:
-                    login(username, password)
+                print("Running cycle ..")
+                print("Checking if logged in ..")
+                if not login_status():
+                    print("Trying to log in ..")
+                    login(url, username, password)
+
+                time.sleep(30)
+
+        elif sys.argv[1] == '--login' or sys.argv[1] == '-l':
+            url = input('Enter login url (eg: http://10.254.254.45/0/up/): ')
+            username = input('Enter username: ')
+            password = getpass.getpass('Enter password: ')
+
+            while(1):
+                print("Running cycle ..")
+                print("Checking if logged in ..")
+                if not login_status():
+                    print("Trying to log in ..")
+                    login(url, username, password)
 
                 time.sleep(30)
         elif sys.argv[1] == '--status' or sys.argv[1] == '-s':
-            service_status(username, password)
+            url = input('Enter login url (eg: http://10.254.254.45/0/up/): ')
+            username = input('Enter username: ')
+            password = getpass.getpass('Enter password: ')
+            service_status(url, username, password)
+
     except KeyboardInterrupt:
         print("Interrupted")
+
+
+if __name__ == '__main__':
+    main()
